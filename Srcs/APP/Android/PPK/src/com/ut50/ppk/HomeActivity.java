fa71dc23fb971c13f64dcbe59d715b2dddf4cbb5
Xing Qianqian <alps_xing@hotmail.com>
2013-03-02 17:15:54 +0800

    Add new choose type dropdown list.

--- Srcs/APP/Android/PPK/src/com/ut50/ppk/HomeActivity.java
+++ Srcs/APP/Android/PPK/src/com/ut50/ppk/HomeActivity.java
@@ -8,11 +8,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class HomeActivity extends FragmentActivity
 						  implements ActivityTypeChooseDialogFragment.ActivityTypeChooseDialogListener, 
@@ -20,10 +27,13 @@ public class HomeActivity extends FragmentActivity
 
 	private static final int NUM_PAGES = 3;
 
-	PPKTabView m_ppkTabView;
-	ViewPager m_viewPager;
+	private PPKTabView m_ppkTabView;
+	private ViewPager m_viewPager;
 	private PagerAdapter m_PagerAdapter;
-	RelativeLayout.LayoutParams savePreviousLayoutParams;
+	private RelativeLayout.LayoutParams m_savePreviousLayoutParams;
+	private LinearLayout m_typeLayout;
+	private ScrollView m_scrollTypeChoose;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,18 +47,19 @@ public class HomeActivity extends FragmentActivity
 		m_viewPager.setAdapter(m_PagerAdapter);
 		m_viewPager.setCurrentItem(0);
 		m_viewPager.setOnPageChangeListener(m_ppkTabView);
-
+		
+		PrepreButtons();
 
 		if(titled){
         	getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.home_title_layout);
         }
 		
 		ImageView previousArrow = (ImageView)this.findViewById(R.id.imageView_previous);
-		savePreviousLayoutParams = (RelativeLayout.LayoutParams)previousArrow.getLayoutParams();
+		m_savePreviousLayoutParams = (RelativeLayout.LayoutParams)previousArrow.getLayoutParams();
 		previousArrow.setLayoutParams(new RelativeLayout.LayoutParams(0, 0));
 		
 		ImageView newActivity = (ImageView)this.findViewById(R.id.imageView_title_new);
-		newActivity.setOnClickListener(clickListener);
+		newActivity.setOnClickListener(newActivityClickListener);
 	}
 
 	@Override
@@ -58,12 +69,20 @@ public class HomeActivity extends FragmentActivity
 		return true;
 	}
 	
-	View.OnClickListener clickListener = new View.OnClickListener() {
+	View.OnClickListener newActivityClickListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v)
 		{
-			ActivityTypeChooseDialogFragment newFragment = new ActivityTypeChooseDialogFragment();
-		    newFragment.show(getSupportFragmentManager(), "ChooseType");
+			//ActivityTypeChooseDialogFragment newFragment = new ActivityTypeChooseDialogFragment();
+		    //newFragment.show(getSupportFragmentManager(), "ChooseType");
+			if(m_scrollTypeChoose.getVisibility() == View.VISIBLE)
+			{
+				HideTypeChooser();
+			}
+			else
+			{
+				PopupTypeChooser();
+			}
 		}
 	};
 
@@ -79,7 +98,78 @@ public class HomeActivity extends FragmentActivity
 		Log.i("HomeActivity::onTabClicked", "Position:" + position);
 		m_viewPager.setCurrentItem(position);
 	}
+	
+	View.OnClickListener typeClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v)
+		{
+			Log.i("HomeActivity::typeClickListener", "Clicked");
+		}
+	};
 
+	View.OnFocusChangeListener typeFocusChangeListener = new View.OnFocusChangeListener() {
+		
+		@Override
+		public void onFocusChange(View v, boolean hasFocus) {
+			Log.i("HomeActivity::typeFocusChangeListener", "Focus " + hasFocus);
+		}
+	};
+
+	private void PrepreButtons()
+	{
+		m_typeLayout = (LinearLayout)this.findViewById(R.id.layout_choose_type);
+		m_scrollTypeChoose = (ScrollView)this.findViewById(R.id.scroll_choose_type);
+		String[] actTypes = this.getResources().getStringArray(R.array.ActType);
+		for(int i = 0; i < actTypes.length; i ++)
+		{
+			if(i > 0)
+			{
+				ImageView imgView = new ImageView(this);
+				imgView.setImageResource(R.drawable.black_line);
+				imgView.setScaleType(ScaleType.FIT_XY);
+				LinearLayout.LayoutParams imgParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0);
+				imgParams.setMargins(5, 0, 5, 0);
+				m_typeLayout.addView(imgView, imgParams);
+			}
+
+			TextView txtView = new TextView(this);
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 0);
+			params.setMargins(5, 0, 5, 0);
+			txtView.setText(actTypes[i]);
+	        txtView.setGravity(Gravity.CENTER);
+	        txtView.setPadding(0, 15, 0, 15);
+	        txtView.setTextSize(20);
+	        txtView.setTextColor(this.getResources().getColor(R.color.dropdown_text_color));
+	        txtView.setOnClickListener(typeClickListener);
+	        m_typeLayout.addView(txtView, params);
+		}
+		
+		m_scrollTypeChoose.setOnFocusChangeListener(typeFocusChangeListener);
+		
+		HideTypeChooser();
+	}
+	
+	private void PopupTypeChooser()
+	{
+		m_scrollTypeChoose.setVisibility(View.VISIBLE);
+	}
+
+	private void HideTypeChooser()
+	{
+		m_scrollTypeChoose.setVisibility(View.INVISIBLE);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if (keyCode == KeyEvent.KEYCODE_BACK) {
+	    	if(m_scrollTypeChoose.getVisibility() == View.VISIBLE)
+	    	{
+	    		HideTypeChooser();
+	    		return true;
+	    	}
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}
 	
 	private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
         public ScreenSlidePagerAdapter(FragmentManager fm) {
